56ecc52807b5f2d418db9243d9afd4916b1bef33
Echoffee <Echoffee@users.noreply.github.com>
2018-02-10 16:37:59 +0100

    Fix GameTest errors

--- src/game/Game.java
+++ src/game/Game.java
@@ -22,51 +22,51 @@ public class Game {
     private IBoardManager boardManager;
     private UserInterface ui;
 
-    public Game(UserInterface ui){
+    public Game(UserInterface ui) {
         this.ui = ui;
         boardManager = BoardManager.getInstance();
         ui.sendResponse(new SharedCommand(REFRESH), boardManager.getBoard());
     }
 
-    public void startNewGame(){
+    public void startNewGame() {
         gameLoop();
     }
 
-    private void initGame(){
+    private void initGame() {
 
     }
 
-    private void gameLoop(){
+    private void gameLoop() {
         boolean quit = false;
-        while (!quit){
+        while (!quit) {
             SharedCommand cmd = ui.getNextCommand();
-            switch (cmd.getCommand()){
-                case EXIT:{
+            switch (cmd.getCommand()) {
+                case EXIT: {
                     quit = true;
                     cmd.setResponse(VALID);
                     break;
                 }
-                case LOAD:{
+                case LOAD: {
                     break;
                 }
-                case SAVE:{
+                case SAVE: {
                     break;
                 }
-                case REVERT:{
+                case REVERT: {
                     break;
                 }
-                case CMD_ERROR:{
+                case CMD_ERROR: {
                     cmd.setResponse(GAME_ERROR);
                     ui.sendResponse(cmd, null);
                     continue;
                 }
-                case PAT:{
+                case PAT: {
                     break;
                 }
-                case SURRENDER:{
+                case SURRENDER: {
                     break;
                 }
-                default:{
+                default: {
                     //if ruleEngine.isValid(cmd)
                     //then boardManager.apply(cmd)
                     //     cmd.setResponse(VALID)
@@ -75,6 +75,7 @@ public class Game {
                         int[] b = cmd.getCoords2();
                         System.out.println("GAME : From [" + a[0] + "," + a[1] + "] To [" + b[0] + "," + b[1] + "]");
                         cmd.setResponse(VALID);
+                        RuleChecker.getInstance().checkAction(BoardManager.getInstance().getBoard(), convert(cmd));
                     } catch (Exception e) {
                         cmd.setResponse(GAME_ERROR);
                     }
@@ -83,6 +84,43 @@ public class Game {
             }
             ui.sendResponse(cmd, boardManager.getBoard());
         }
+    }
+
+    //Wrapper for SharedCommand -> GameAction while not sure about final design layout
+    private GameAction convert(SharedCommand cmd) {
+        EGameActionType actionType = EGameActionType.NONE;
+        switch (cmd.getCommand()) {
+
+            case MOVE:
+                actionType = EGameActionType.MOVE;
+                break;
+            case ATTACK:
+                actionType = EGameActionType.ATTACK;
+                break;
+            case CHARGE:
+                actionType = EGameActionType.CHARGE;
+                break;
+            case PAT:
+                actionType = EGameActionType.PROPOSE_DRAW;
+                break;
+            default:
+                return null;
+        }
+
+        GameAction result = new GameAction(EPlayer.PLAYER1 /*TODO: placeholder*/, actionType);
+        try {
+            result.setSourceCoordinates(cmd.getCoords1()[0], cmd.getCoords1()[1]);
+        } catch (CommandException e) {
+            result.setSourceCoordinates(1, 1);
+        }
+
+        try {
+            result.setTargetCoordinates(cmd.getCoords2()[0], cmd.getCoords2()[1]);
+        } catch (CommandException e) {
+            result.setTargetCoordinates(1, 1);
+        }
+
+        return result;
     }
 
 }
