c744587e632eb8bdd23c296bc442ca87d65fe6b4
Echoffee <Echoffee@users.noreply.github.com>
2018-03-24 02:32:38 +0100

    Reduce map names length + Minor tweaks on visuals

--- src/analyse/EMetricsMapType.java
+++ src/analyse/EMetricsMapType.java
@@ -4,30 +4,25 @@ import analyse.mapMethods.*;
 import javafx.scene.paint.Paint;
 
 public enum EMetricsMapType {
-	COMMUNICATION_MAP("Communication map", new CommunicationMapMethod()),
+	COMMUNICATION_MAP   ("Communication map", new CommunicationMapMethod()),
 
-    STATIC_ATTACK_MAP("Static Attack Map", new StaticAttackMapMethod()),
-    ATTACK_MAP_1M("Attack map using 1 unit (slow)", new DeepAttackMapMethod(1)),
-    ATTACK_MAP_FAST("Imprecise Attack Map (fast)", new ImpreciseAttackMapMethod()),
+    STATIC_ATTACK_MAP   ("Attack Map (static)", new StaticAttackMapMethod()),
+    ATTACK_MAP_1M       ("Attack Map Deep 1 (slow)", new DeepAttackMapMethod(1)),
+    ATTACK_MAP_FAST     ("Attack Map Low (fast)", new ImpreciseAttackMapMethod()),
 
-	RANGE_MAP_1M("Range map using 1 unit (slow)", new DeepRangeMapMethod(1)),
-	RANGE_MAP_FAST("Imprecise Range Map (fast)", new ImpreciseRangeMapMethod()),
+	RANGE_MAP_1M        ("Range Map Deep 1 (slow)", new DeepRangeMapMethod(1)),
+	RANGE_MAP_FAST      ("Range Map Low (fast)", new ImpreciseRangeMapMethod()),
 
-	DEFENSE_MAP_FAST("Imprecise Defense Map (fast)", new ImpreciseDefenseMapMethod()),
+	DEFENSE_MAP_FAST    ("Defense Map Low (fast)", new ImpreciseDefenseMapMethod()),
 
-	OUTCOME_MAP_FAST("Imprecise Outcome Prediction Map (fast)", new ImpreciseOutcomePredictionMapMethod());
+	OUTCOME_MAP_FAST	("Outcome Map Low (fast)", new ImpreciseOutcomePredictionMapMethod());
 
 	private final String mapName;
     private final IMetricsMapMethod method;
-    private final static int maxIndex = 7;
 
     EMetricsMapType(String mapName, IMetricsMapMethod metricMethod){
         this.mapName = mapName;
     	this.method = metricMethod;
-	}
-
-	public static int getMaxIndex(){
-    	return maxIndex;
 	}
 
     public IMetricsMapMethod getMethod() {
@@ -37,10 +32,6 @@ public enum EMetricsMapType {
     public Paint getPaint(double value) {
         return method.getPaint(value);
     }
-
-	public String getMapName() {
-		return mapName;
-	}
 
 	@Override
 	public String toString() {
--- src/ui/UIElements/InfosPane.java
+++ src/ui/UIElements/InfosPane.java
@@ -1,10 +1,8 @@
 package ui.UIElements;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
@@ -17,17 +15,18 @@ class InfosPane extends VBox {
     public InfosPane(ChoiceBox metricsDisplay, CheckBox metricsChannel_1, CheckBox metricsChannel_2){
         this.setBackground(new Background(new BackgroundFill(Color.WHITESMOKE, null, null)));
         this.setMinWidth(MainLayout.INFOS_WIDTH);
-
+		this.setAlignment(Pos.TOP_CENTER);
         metricsDisplay.getSelectionModel().selectFirst();
         this.getChildren().add(metricsDisplay);
         HBox checks = new HBox();
         checks.setAlignment(Pos.CENTER);
+        checks.setSpacing(5);
+		checks.setPadding(new Insets(5, 5, 5, 5));
         this.getChildren().add(checks);
         checks.getChildren().add(metricsChannel_1);
         checks.getChildren().add(metricsChannel_2);
 
         this.getChildren().add(new Label("\nLegend : "));
-
         for(EUnitData unit : EUnitData.values()){
             IconTitledPane tp = new IconTitledPane(unit.getDisplayName(), unit);
             tp.setExpanded(false);
