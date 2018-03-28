1b079da38e437d240d02fd29fc569e5ce470ebcb
Echoffee <Echoffee@users.noreply.github.com>
2018-03-28 20:32:48 +0200

    Rename analysis map names

--- src/analyse/EMetricsMapType.java
+++ src/analyse/EMetricsMapType.java
@@ -7,15 +7,15 @@ public enum EMetricsMapType {
     COMMUNICATION_MAP("Communication map", new CommunicationMapMethod()),
 
     STATIC_ATTACK_MAP("Attack Map (static)", new StaticAttackMapMethod()),
-    ATTACK_MAP_1M("Attack Map Deep 1 (slow)", new DeepAttackMapMethod(1)),
-    ATTACK_MAP_FAST("Attack Map Low (fast)", new ImpreciseAttackMapMethod()),
+    ATTACK_MAP_1M("Attack Map Deep 1 move", new DeepAttackMapMethod(1)),
+    ATTACK_MAP_FAST("Attack Map Low Res", new ImpreciseAttackMapMethod()),
 
-    RANGE_MAP_1M("Range Map Deep 1 (slow)", new DeepRangeMapMethod(1)),
-    RANGE_MAP_FAST("Range Map Low (fast)", new ImpreciseRangeMapMethod()),
+    RANGE_MAP_1M("Range Map Deep 1 move", new DeepRangeMapMethod(1)),
+    RANGE_MAP_FAST("Range Map Low Res", new ImpreciseRangeMapMethod()),
 
-    DEFENSE_MAP_FAST("Defense Map Low (fast)", new ImpreciseDefenseMapMethod()),
+    DEFENSE_MAP_FAST("Defense Map Low Res", new ImpreciseDefenseMapMethod()),
 
-    OUTCOME_MAP_FAST("Outcome Map Low (fast)", new ImpreciseOutcomePredictionMapMethod());
+    OUTCOME_MAP_FAST("Outcome Map Low Res", new ImpreciseOutcomePredictionMapMethod());
 
     private final String mapName;
     private final IMetricsMapMethod method;
